7f823193ec26cc6e2e0e6c6ad3e31945be4100cc
Tales Tonini <talestonini@gmail.com>
2015-06-25 17:20:55 +1000

    Adding test for domain class Game

--- bfapi/grails-app/domain/button/football/Game.groovy
+++ bfapi/grails-app/domain/button/football/Game.groovy
@@ -33,6 +33,6 @@ class Game {
 
     @Override
     String toString() {
-        "$homeTeam.name $numHomeTeamGoals x $awayTeam.name $numAwayTeamGoals, $gameType, $championship.championshipType ed $championship.numEdition"
+        "$homeTeam.name $numHomeTeamGoals x $awayTeam.name $numAwayTeamGoals, $gameType, $championship"
     }
 }
