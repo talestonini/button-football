662de7272722e2770ed1d2a913cf1fea21a22161
Tales Tonini <talestonini@gmail.com>
2015-11-04 00:38:47 +1100

    Upgrading components versions and setting default excerpt projection to team repo

--- api/src/main/groovy/button/football/domain/repo/TeamRepository.groovy
+++ api/src/main/groovy/button/football/domain/repo/TeamRepository.groovy
@@ -2,12 +2,15 @@ package button.football.domain.repo
 
 import button.football.domain.model.Team
 import button.football.domain.model.TeamType
+import button.football.domain.model.projection.TeamProjection
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.query.Param
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
 /**
  * Created by talestonini on 29/08/2015.
  */
+@RepositoryRestResource(excerptProjection = TeamProjection.class)
 interface TeamRepository extends JpaRepository<Team, Long> {
 
     Set<TeamType> findByTeamTypeId(@Param(value = 'teamTypeId') Long teamTypeId)
