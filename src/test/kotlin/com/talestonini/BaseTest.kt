800ac606416fdc83eaeb2e86295c33a4c3fa526c
Tales Tonini <6622432+talestonini@users.noreply.github.com>
2025-06-23 22:42:07 +1000

    Testing database final standings (cont...)

--- src/test/kotlin/com/talestonini/BaseTest.kt
+++ src/test/kotlin/com/talestonini/BaseTest.kt
@@ -8,14 +8,9 @@ abstract class BaseTest {
     companion object {
         private var db: Database? = null
 
-        fun database(): Database? {
+        fun database(url: String = "jdbc:h2:./h2/db/buttonfootball"): Database? {
             if (db == null) {
-                db = Database.connect(
-                    url = "jdbc:h2:./h2/db/buttonfootball",
-                    driver = "org.h2.Driver",
-                    user = "sa",
-                    password = "buttonfootball"
-                )
+                db = Database.connect(url, driver = "org.h2.Driver", user = "sa", password = "buttonfootball")
             }
             return db
         }
