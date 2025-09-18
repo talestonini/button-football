03f4481302bdedbed8638215e7a442142375f7ad
Tales Tonini <6622432+talestonini@users.noreply.github.com>
2025-09-18 22:52:57 +1000

    Validations

--- src/main/kotlin/com/talestonini/service/sorting/RankingSortingService.kt
+++ src/main/kotlin/com/talestonini/service/sorting/RankingSortingService.kt
@@ -1,11 +1,32 @@
 package com.talestonini.service.sorting
 
 import com.talestonini.service.Ranking
+import com.talestonini.service.Standing
 
 class RankingSortingService {
     companion object {
 
-        fun processRanking(codChampionshipType: String, numUpToEdition: Int): Set<Ranking> {
+        /**
+         * Processes the ranging of a championship type given the set of final standings to be computed.  For example,
+         * if wanting to obtain the ranking at the end of the third edition of a championship type, then the set of
+         * final standings must contain the final standings of the 3 first championships of that championship type.  In
+         * that example, a team that participated in all 3 editions should have 3 entries in the set.
+         */
+        fun processRanking(finalStandings: Set<Standing>): Set<Ranking> {
+            // all standings are of the same championship type
+            val championshipType = finalStandings.first().championship.type
+            finalStandings.all { it.championship.type == championshipType }
+            // editions start in 1 and have no gaps
+            val editions = finalStandings.map { it.championship.numEdition }.distinct().sorted()
+            assert(editions.min() == 1)
+            val maxEdition = editions.max()
+            assert(editions.size == maxEdition)
+            // there are no missing standings
+            for (i in 1..maxEdition) {
+                val iStandings = finalStandings.filter { it.championship.numEdition == i }
+                assert(iStandings.size == iStandings.first().championship.numTeams)
+            }
+
             return emptySet()
         }
 
--- src/test/kotlin/com/talestonini/service/sorting/RankingSortingServiceTest.kt
+++ src/test/kotlin/com/talestonini/service/sorting/RankingSortingServiceTest.kt
@@ -3,8 +3,10 @@ package com.talestonini.service.sorting
 import com.talestonini.data.BaseDataTest
 import com.talestonini.model.ChampionshipTypeEntity
 import com.talestonini.model.RankingEntity
+import com.talestonini.model.StandingEntity
 import com.talestonini.service.Ranking
 import com.talestonini.service.RankingService
+import com.talestonini.service.StandingService
 import com.talestonini.service.sorting.RankingSortingService.Companion.processRanking
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -15,10 +17,18 @@ class RankingSortingServiceTest : BaseDataTest() {
     fun `ranking should have teams in their correct position`() = dataTest {
         ChampionshipTypeEntity.all().sortedBy { it.id.value }
             .forEach { ct ->
-                for (i in 0..ct.numEditions) {
+                for (i in 1..ct.numEditions) {
                     println(">>> testing championship type '${ct.code}', up to edition $i")
 
-                    val calcRanking = processRanking(ct.code, i)
+                    val finalStandings = StandingEntity.all()
+                        .filter { it.championship.type == ct &&
+                                    it.championship.numEdition <= i &&
+                                    it.matchType.code.lowercase() == "f1"
+                        }
+                        .map { StandingService.toStanding(it) }
+                        .toSet()
+
+                    val calcRanking = processRanking(finalStandings)
 
                     val dbRanking = RankingEntity.all()
                         .filter { it.championshipType == ct && it.numUpToEdition == i }
